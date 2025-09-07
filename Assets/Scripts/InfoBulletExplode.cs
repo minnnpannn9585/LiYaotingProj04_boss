ba2d3b816e7da4b6d12b3807ac8451686ea331dc
minnnpannn9585 <panmin677095@gmail.com>
2025-09-07 09:45:55 +0800

    0907

--- Assets/Scripts/InfoBulletExplode.cs
+++ Assets/Scripts/InfoBulletExplode.cs
@@ -5,7 +5,6 @@ public class ExplosionVisualOnly : MonoBehaviour
 {
     public float BulletLiveTime;
     public GameObject particlePrefab;
-    public AudioClip explosionSound;
     public float particleDuration = 3f;
 
     void Start()
@@ -21,12 +20,6 @@ public class ExplosionVisualOnly : MonoBehaviour
             GameObject v = Instantiate(particlePrefab, transform.position, Quaternion.identity);
             Destroy(v, particleDuration);
         }
-
-        if (explosionSound != null)
-            AudioSource.PlayClipAtPoint(explosionSound, transform.position);
-
-        // 发送消息，供其他脚本处理（可改为事件系统）
-        SendMessage("OnExploded", SendMessageOptions.DontRequireReceiver);
 
         Destroy(gameObject);
     }
